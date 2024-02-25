7f9b4dad65707f2474d1a62691a7ef464ea42a13
Dmitry Shein <vailor1488@yandex.ru>
2024-02-25 11:31:43 +0400

    Initial Commit

---
+++ ETM_Shop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ETM_Shop
+//
+//  Created by Дмитрий Шеин on 25.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ETM_Shop/ETM_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ETM_ShopApp.swift
+//  ETM_Shop
+//
+//  Created by Дмитрий Шеин on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ETM_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
